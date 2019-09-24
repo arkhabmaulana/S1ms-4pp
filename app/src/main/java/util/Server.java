5a86787b049d5e160265487fc53a3c8ad4f7b2c9
lanasinergy <arkhabmaulana1@gmail.com>
2019-09-24 15:58:29 +0700

    [Update] Server file

--- app/src/main/java/util/Server.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Server {
-
-    public static final String URL = "http://192.168.2.142/api/login";
-
-    public static final String saveData = "http://192.168.2.142/api/store";
-
-}
---
+++ app/src/main/java/util/Server.kt
@@ -0,0 +1,9 @@
+package util
+
+object Server {
+
+    val URL = "http://192.168.2.142/api/login"
+
+    val saveData = "http://192.168.2.142/api/store"
+
+}
